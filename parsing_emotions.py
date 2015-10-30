29108fa7668ffcd098f64631b07879817afabc14
Lucas Png <clp93@cornell.edu>
2015-10-29 22:44:00 -0400

    created a global tuple tweettexts and a funciton that takes in a twitter user name and returns the first 5 tweets and replies

--- parsing_emotions.py
+++ parsing_emotions.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python
 
-#Open emotions.txt file and build up the sets
+import tweepy
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key="BxEa0a7otNC7IvIQG65ir8Yqm"
+consumer_secret="XOfwYzc7mD3RABGRuHWeD2z2rXqTtJt6FY474dKXcJj5nv8vDm"
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located
+# under "Your access token")
+access_token="1337652349-nTJAw5CQWK7l1SJkD1XlOBcAqQbPeNRRi6JDyZ0"
+access_token_secret="NDPVz04sjw0Vgn2xy9n7AnKRgkndJnEwSCVBDCmQfZHPq"
+
+tweetexts = ()
+
+def get_five_tweets(screen_name):
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.secure = True
+  auth.set_access_token(access_token, access_token_secret)
+
+  api = tweepy.API(auth)
+
+  # If the authentication was successful, you should
+  # see the name of the account print out
+  alltweets = api.user_timeline(screen_name = screen_name,count=5)
+  i=0
+  for p in alltweets: 
+    tweettexts = tweettexts + (p.text,)
+
 emotionsFile = open("emotions.txt", "r")
 
 #Emotions
@@ -64,6 +92,7 @@ def classifyIntoSet(word, senti, flag):
 	return;
 
 #Iterate through each line in the file and add the words to the sets
+
 for i in range(numLines):
 	toParse = linesInFile[i].split()
 	classifyIntoSet(toParse[0], toParse[1], toParse[2])
